598abe86f3cbdb4fc2e019c378bea00de0ff665e
Stuart Hutson <stu.hutson@gmail.com>
2015-06-15 23:20:18 +0100

    alana test bool

---
+++ Assets/Test.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+
+    float x = 1.2f;
+    
+    double y = 1.2;
+
+    int a = 2;
+
+    string hello = "hello";
+
+    bool g = true;
+
+    bool alana = false;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Debug.Log("Test");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Test2.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test2 : MonoBehaviour {
+
+
+    bool stu = false;
+
+    float alana = 1.2f;
+
+	// Use this for initialization
+	void Start () 
+
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
